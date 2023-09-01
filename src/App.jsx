166b2e8549459c1da4f1d2290e5da3358e884392
Samind2 <111554627+Samind2@users.noreply.github.com>
2023-09-01 11:48:17 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Post from "./component/post";
+import PostFunction from "./component/PostFunction";
+import Comment from "./component/Comment";
+
+function App() {
+  return (
+    <>
+      <Post userId="Supanee" message="แย่จัง">
+        <Comment userId="Supanee" message="หิวข้าววววว" />
+      </Post>
+      <PostFunction />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/PostFunction.jsx
@@ -0,0 +1,10 @@
+const PostFunction = () => {
+    return (
+        <div>
+                <h2>Function Component</h2>
+                <p>Supanee</p>
+            </div>
+    );
+};
+
+export default PostFunction;
